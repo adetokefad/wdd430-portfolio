452359151f483c1592c97707de79349c53aff960
adetokefad <adetokefaderin@gmail.com>
2026-07-15 21:40:36 +0100

    Fix component import paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "@/components/Header.tsx";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
